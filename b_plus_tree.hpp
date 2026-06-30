103f559b06448f785da95c7b5ab28b902a97ebd0
kiwigenio <kerinlarico@gmail.com>
2026-06-29 19:47:09 -0500

    agregar las funciones necesarias para la eliminacion en un arbol b+

--- b_plus_tree.hpp
+++ b_plus_tree.hpp
@@ -3,12 +3,25 @@
 #include "b_plus_tree_internal_page.hpp"
 #include "b_plus_tree_leaf_page.hpp"
 #include <vector>
+#include <queue>
 
 template<typename KeyType>
 class BPlusTree{
     private: 
         BufferPoolManager *bpm_;
         int32_t root_page_id_;
+        Page* FindLeafPage(const KeyType &key);
+
+        bool AdjustRoot(BPlusTreePage *node);
+        
+        template <typename N>
+        bool CoalesceOrRedistribute(N *node);
+
+        template <typename N>
+        bool Coalesce(N **neighbor_node, N **node, BPlusTreeInternalPage<KeyType> **parent, int index);
+
+        template <typename N>
+        void Redistribute(N *neighbor_node, N *node, int index);
     public:
         BPlusTree(BufferPoolManager *bpm): bpm_(bpm), root_page_id_(-1){}
 
@@ -17,6 +30,12 @@ class BPlusTree{
         bool GetValue(const KeyType &key, RID *result);
 
         bool Insert(const KeyType &key, const RID &value);
-    private: 
-        Page* FindLeafPage(const KeyType &key);
+
+        void Remove(const KeyType &key);
+
+        void PrintTree();
+
+        int32_t GetRootPageId() const { return root_page_id_; }
+        void SetRootPageId(int32_t root_id) { root_page_id_ = root_id; }
+
 };
